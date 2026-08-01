6f3b48208ec6eddcb29d9c53bb7bf7ba5933e015
MARCO <jmukhammadsodikh@gmail.com>
2026-08-01 21:10:37 +0900

    feat: publish auth modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,17 +9,23 @@ import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footers";
-import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css";
 import HelpPage from "./screens/helpPage";
 import Test from "./screens/Test";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
-
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /** Handlers */
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return <>
     {location.pathname === "/" ?
@@ -43,6 +49,13 @@ function App() {
       </Route>
     </Switch>
     <Footer />
+
+    <AuthenticationModal
+      signupOpen={signupOpen}
+      loginOpen={loginOpen}
+      handleLoginClose={handleLoginClose}
+      handleSignupClose={handleSignupClose}
+    />
   </>
 }
 
